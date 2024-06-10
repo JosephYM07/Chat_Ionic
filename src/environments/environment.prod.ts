6dce86f70ae1276f9c9b2d3471f479c6a47c070e
JosephYM07 <josepym07@gmail.com>
2024-06-10 10:22:45 -0500

    Proyecto final

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBoO8gEJo-VQFfPS2aY7xdu8mVVbdU5dAI",
+    authDomain: "chat-7135c.firebaseapp.com",
+    projectId: "chat-7135c",
+    storageBucket: "chat-7135c.appspot.com",
+    messagingSenderId: "386282013818",
+    appId: "1:386282013818:web:2570886e609822151ca7c3"
+  }
+};
